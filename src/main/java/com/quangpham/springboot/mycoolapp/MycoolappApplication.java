83181abcab0c1df7ac6719024f70892c6e4dc27a
quang-pham <nhutquang23@gmail.com>
2023-04-29 13:12:20 +0700

    Create REST controller

--- src/main/java/com/quangpham/springboot/mycoolapp/MycoolappApplication.java
+++ src/main/java/com/quangpham/springboot/mycoolapp/MycoolappApplication.java
@@ -2,8 +2,10 @@ package com.quangpham.springboot.mycoolapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.quangpham.springboot")
 public class MycoolappApplication {
 
 	public static void main(String[] args) {
