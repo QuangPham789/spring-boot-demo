263e4e49363f1d2e2da17b3374c4824a8c4a46ce
quang-pham <nhutquang23@gmail.com>
2023-05-01 11:35:12 +0700

    custom application proerties file

--- src/main/java/com/quangpham/springboot/controller/HomeController.java
+++ src/main/java/com/quangpham/springboot/controller/HomeController.java
@@ -1,14 +1,17 @@
 package com.quangpham.springboot.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
 
+    @Value("${app.messages}")
+    private String message;
     @GetMapping("/")
     public String home(){
-        return "Hello home";
+        return message;
     }
 
     @GetMapping("/workout")
